795c8a4ca872943a07719140f82d64ec3641b750
Karahan <68694262+KarahanKARA@users.noreply.github.com>
2022-09-17 03:19:32 +0300

    Engine sound added

---
+++ DragRacing/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,36 @@
+using Player;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip engineSoundClip;
+
+    private CarController _carController;
+    private AudioSource _audioSource;
+    private float _maxRpm;
+    
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.clip = engineSoundClip;
+        _audioSource.pitch = 0.43f;
+    }
+
+    private void Start()
+    {
+        _carController = GameObject.FindGameObjectWithTag("Player").GetComponent<CarController>();
+        _maxRpm = _carController.GetMaxRpm();
+    }
+
+    private void Update()
+    {
+        var currentRpm = _carController.CurrentRpm;
+        _audioSource.pitch = ((currentRpm/_maxRpm)*1.6f)+0.4f;
+
+        if (!_audioSource.isPlaying)
+        {
+            _audioSource.Play();
+        }
+
+    }
+}
